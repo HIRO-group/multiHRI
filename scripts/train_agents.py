a346894a830d8a933b56cdeacaff763a184bdef4
Joe Miceli <joe.miceli8@gmail.com>
2024-08-28 15:58:04 -0600

    configuration for N-1-SP vs Baselines experiment

--- scripts/train_agents.py
+++ scripts/train_agents.py
@@ -84,30 +84,30 @@ def SP_w_SP_Types(args,
 
 
 def FCP(args, pop_force_training, fcp_force_training, parallel):
-    args.fcp_train_types = [TeamType.LOW_FIRST, TeamType.MEDIUM_FIRST, TeamType.HIGH_FIRST]
-    args.fcp_eval_types = {'generate' : [],
-                            'load': get_eval_types_to_load()}
+    args.fcp_train_types = [TeamType.RANDOM]
+    args.fcp_eval_types = {'generate' : [TeamType.LOW_FIRST, TeamType.HIGH_FIRST, TeamType.MEDIUM_FIRST],
+                            'load': []}
+    fcp_curriculum = Curriculum(train_types=args.fcp_train_types, is_random=True)
 
-    fcp_curriculum = Curriculum(train_types = args.fcp_train_types,
-                                is_random=False,
-                                total_steps = args.fcp_total_training_timesteps//args.epoch_timesteps,
-                                training_phases_durations_in_order={
-                                    TeamType.LOW_FIRST: 0.5,
-                                    TeamType.MEDIUM_FIRST: 0.125,
-                                    TeamType.HIGH_FIRST: 0.125,
-                                },
-                                rest_of_the_training_probabilities={
-                                    TeamType.LOW_FIRST: 0.4,
-                                    TeamType.MEDIUM_FIRST: 0.3, 
-                                    TeamType.HIGH_FIRST: 0.3,
-                                },
-                                probabilities_decay_over_time=0
-                            )
+    # fcp_curriculum = Curriculum(train_types = args.fcp_train_types,
+    #                             is_random=False,
+    #                             total_steps = args.fcp_total_training_timesteps//args.epoch_timesteps,
+    #                             training_phases_durations_in_order={
+    #                                 TeamType.LOW_FIRST: 0.5,
+    #                                 TeamType.MEDIUM_FIRST: 0.125,
+    #                                 TeamType.HIGH_FIRST: 0.125,
+    #                             },
+    #                             rest_of_the_training_probabilities={
+    #                                 TeamType.LOW_FIRST: 0.4,
+    #                                 TeamType.MEDIUM_FIRST: 0.3, 
+    #                                 TeamType.HIGH_FIRST: 0.3,
+    #                             },
+    #                             probabilities_decay_over_time=0
+    #                         )
 
     _, _ = get_fcp_agent_w_tms_clction(args,
                                         pop_total_training_timesteps=args.pop_total_training_timesteps,
                                         fcp_total_training_timesteps=args.fcp_total_training_timesteps,
-                                        fcp_train_types=args.fcp_train_types,
                                         fcp_eval_types=args.fcp_eval_types,
                                         pop_force_training=pop_force_training,
                                         fcp_force_training=fcp_force_training,
@@ -150,25 +150,28 @@ def set_input(args, quick_test=False):
     '3_chefs_counter_circuit', '3_chefs_asymmetric_advantages', 
     '3_chefs_forced_coordination_3OP2S1D'.
     '''
-    args.layout_names = ['3_chefs_small_kitchen']
+    args.layout_names = ['3_chefs_small_kitchen_two_resources', 
+                         '3_chefs_asymmetric_advantages', 
+                         '3_chefs_counter_circuit', 
+                         '3_chefs_forced_coordination_3OP2S1D']
     args.teammates_len = 2
     args.num_players = args.teammates_len + 1  # 3 players = 1 agent + 2 teammates
     args.learner_type = LearnerType.SUPPORTER
     
     if not quick_test: 
-        args.n_envs = 50
+        args.n_envs = 100
         args.epoch_timesteps = 1e5
-        how_long = 1.0
+        how_long = 2.0
         args.pop_total_training_timesteps = 5e6 * how_long
         args.fcp_total_training_timesteps = 2 * 5e6 * how_long
         args.sp_w_sp_total_training_timesteps = 5e6 * how_long
         args.fcp_w_sp_total_training_timesteps = 4 * 5e6 * how_long        
         args.SP_seed, args.SP_h_dim = 68, 256
-        args.SPWSP_seed, args.SPWSP_h_dim = 1010, 256
+        args.SPWSP_seed, args.SPWSP_h_dim = 2020, 256
         args.FCP_seed, args.FCP_h_dim = 2020, 256
         args.FCPWSP_seed, args.FCPWSP_h_dim = 2602, 256
         args.num_sp_agents_to_train = 3
-        args.exp_dir = 'experiment-1'
+        args.exp_dir = 'all_layouts_supporters'
 
 
     else: # Used for doing quick tests
@@ -189,20 +192,20 @@ if __name__ == '__main__':
     quick_test = False
     parallel = True
     
-    pop_force_training = True
-    fcp_force_training = True
+    pop_force_training = False
+    fcp_force_training = False
     fcp_w_sp_force_training = True
     sp_w_sp_force_training = True
     
     set_input(args=args, quick_test=quick_test)
 
-    SP(args=args,
-       pop_force_training=pop_force_training)
+    # SP(args=args,
+    #    pop_force_training=pop_force_training)
 
-    # FCP(args=args,
-    #     pop_force_training=pop_force_training,
-    #     fcp_force_training=fcp_force_training,
-    #     parallel=parallel)
+    FCP(args=args,
+        pop_force_training=pop_force_training,
+        fcp_force_training=fcp_force_training,
+        parallel=parallel)
 
     # SP_w_SP_Types(args=args,
     #               pop_force_training=pop_force_training,
