10adc960ee7daa302d272e1babee9d1d86f46605
Ava Abderezaei <ava.abderezaei@gmail.com>
2025-01-22 19:21:29 -0700

    Add 5-sdlmh

--- scripts/train_agents.py
+++ scripts/train_agents.py
@@ -345,7 +345,7 @@ if __name__ == '__main__':
     args.pop_force_training = False
     args.adversary_force_training = False
     args.primary_force_training = False
-    args.teammates_len = 1
+    args.teammates_len = 4
 
     if args.teammates_len == 1:
         args.how_long = 20
