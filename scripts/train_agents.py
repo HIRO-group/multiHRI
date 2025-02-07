36cf29f5fbbce0ad90e713e3e1965fde4fb3b0a3
Ava Abderezaei <ava.abderezaei@gmail.com>
2025-02-07 15:56:28 -0700

    Add 5-dlmh 2602

--- scripts/train_agents.py
+++ scripts/train_agents.py
@@ -237,13 +237,13 @@ def SPN_XSPCKP(args) -> None:
         TeamType.SELF_PLAY_MEDIUM,
         TeamType.SELF_PLAY_LOW,
         TeamType.SELF_PLAY_DYNAMIC_ADV,
-        TeamType.SELF_PLAY_STATIC_ADV,
+        # TeamType.SELF_PLAY_STATIC_ADV,
     ]
     primary_eval_types = {
         'generate': [TeamType.SELF_PLAY_HIGH,
                      TeamType.SELF_PLAY_LOW,
                      TeamType.SELF_PLAY_DYNAMIC_ADV,
-                     TeamType.SELF_PLAY_STATIC_ADV,
+                    #  TeamType.SELF_PLAY_STATIC_ADV,
                     ],
         'load': []
     }
