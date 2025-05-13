4fdba3831f2e1833a6a42066c98335c1e4366d01
Jmiceli17 <joe.miceli8@gmail.com>
2025-05-13 10:40:08 -0700

    commands to run AB test

--- scripts/run_overcooked_game.py
+++ scripts/run_overcooked_game.py
@@ -17,13 +17,14 @@ if __name__ == "__main__":
     args = get_arguments()
     args.num_players = 2
 
-    args.layout = 'secret_heaven'
+    args.layout = 'c4'
     args.p_idx = 1
     args.layout_names = [args.layout]
     args.n_envs = 1
 
     teammates_path = [
-        'agent_models/RSS_MRS/Eval/ComplexTest/2/SP_hd256_seed3031/ck_0',
+        'agent_models/AB_test/c4/B/best',
+        # 'agent_models/RSS_MRS/Eval/ComplexTest/2/SP_hd256_seed3031/ck_0',
         # 'agent_models/ALMH_CUR/2/SP_hd64_seed14/best', # green
         # 'agent_models/ALMH_CUR/2/SP_hd64_seed14/best', # orange
         # 'agent_models/ALMH_CUR/2/SP_hd64_seed14/best',
@@ -44,10 +45,10 @@ if __name__ == "__main__":
     # player_path = 'agent_models/ALMH_CUR/2/SP_hd64_seed14/best'
     # player_path = 'agent_models/RSS_MRS/Training/Complex/5/N-1-SP_s1010_h256_tr[SPH_SPM_SPL]_ran_originaler/best'
     # player_path = 'agent_models/RSS_MRS/Training/Complex/2/N-1-SP_s1010_h256_tr[SPH_SPM_SPL]_ran_originaler/best'
-    player_path = 'agent_models/RSS_MRS/Training/Complex/2/SP_hd256_seed1010/best'
-    player = load_agent(Path(player_path), args)
+    # player_path = 'agent_models/RSS_MRS/Training/Complex/2/SP_hd256_seed1010/best'
+    # player = load_agent(Path(player_path), args)
     # player = teammates[0]
-    # player = 'human' # blue
+    player = 'human' # blue
 
     dc = OvercookedGUI(args, agent=player, teammates=teammates, layout_name=args.layout, p_idx=args.p_idx, fps=10,
                         horizon=400, gif_name=args.layout)
